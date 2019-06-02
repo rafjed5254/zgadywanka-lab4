b5cc3ec3839e429c3d33b783a2f15305baa8175d
rafjed5254 <48649956+rafjed5254@users.noreply.github.com>
2019-06-02 14:56:41 +0200

    Model gry

---
+++ GraZaDuzoZaMalo/UnitTestProjectModelGry/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectModelGry")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectModelGry")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8a239064-91f4-4417-8137-addb2cd95cae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GraZaDuzoZaMalo/UnitTestProjectModelGry/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelGry;
+
+namespace UnitTestProjectModelGry
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [DataRow(1, 10)]
+        [DataRow(1, 1)]
+        [DataRow(10, 1)]
+        public void Losuj_Zakres_OK(int a, int b)
+        {
+            // AAA
+            // Arrange
+            int x = a;
+            int y = b;
+
+            // Act
+            int los = Gra.Losuj(x, y);
+
+            // Assert
+            Assert.IsTrue(los >= Math.Min(x,y) && los <= Math.Max(x,y) );
+        }
+    }
+}
